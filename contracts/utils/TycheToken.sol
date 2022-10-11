d9622ce49ce6175f2f1768e36d891b3c880823df
Remix Workspace <>
2022-10-11 12:46:13 +0100

    Small update.

--- contracts/utils/TycheToken.sol
+++ contracts/utils/TycheToken.sol
@@ -8,4 +8,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 * @author Anthony (fps) https://github.com/0xfps.
 * @dev Tyche Reward ERC20 Token.
 */
-contract TycheToken {}
+contract TycheToken is ERC20 {
+    constructor(address _address) ERC20("TycheToken", "$TYCHE") {
+        _mint(_address, (1 * 10e9 * (10e18)));
+    }
+}
